3504bf18fa322d924ec7ca65e1bca54727363606
Jon Merrifield <jon@Jons-MacBook-Pro.local>
2019-10-29 13:21:35 -0700

    Add new trail difficulty enum

--- src/resolvers/Trail.js
+++ src/resolvers/Trail.js
@@ -1,4 +1,24 @@
 export default {
   accessedByLifts: (parent, args, { lifts }) =>
-    parent.lift.map(id => lifts.find(l => id === l.id))
+    parent.lift.map(id => lifts.find(l => id === l.id)),
+
+  newDifficulty: ({ difficulty }) => {
+    switch(difficulty) {
+    case "beginner":
+      return "GREEN";
+      
+    case "intermediate":
+      return "BLUE";
+    
+
+    case "advanced":
+    return "BLUE_DIAMOND";
+
+    case "expert":
+      return "BLACK_DIAMOND";
+
+    default:
+      throw new Error();
+    }
+  }
 };
